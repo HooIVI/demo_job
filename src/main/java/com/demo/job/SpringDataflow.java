31040deb7bcf669b79144803c8e10d2225039358
HooIVI <hoo910@hotmail.com>
2017-04-16 01:05:47 +0800

    temp

--- src/main/java/com/demo/job/SpringDataflow.java
+++ src/main/java/com/demo/job/SpringDataflow.java
@@ -10,33 +10,37 @@ import com.dangdang.ddframe.job.api.ShardingContext;
 import com.dangdang.ddframe.job.api.dataflow.DataflowJob;
 import com.demo.entity.Foo;
 import com.demo.process.FooRepository;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 public class SpringDataflow implements DataflowJob<Foo> {
 
-	@Resource
-	private FooRepository fooRepository;
+    Logger logger = LogManager.getLogger(SpringDataflow.class);
 
-	@Override
-	public List<Foo> fetchData(final ShardingContext shardingContext) {
-		System.out.println(String.format(
-				"Item: %s | Time: %s | Thread: %s | %s", shardingContext
-						.getShardingItem(), new SimpleDateFormat("HH:mm:ss")
-						.format(new Date()), Thread.currentThread().getId(),
-				"DATAFLOW FETCH"));
-		return fooRepository.findTodoData(
-				shardingContext.getShardingParameter(), 10);
-	}
+    @Resource
+    private FooRepository fooRepository;
 
-	@Override
-	public void processData(final ShardingContext shardingContext,
-			final List<Foo> data) {
-		System.out.println(String.format(
-				"Item: %s | Time: %s | Thread: %s | %s", shardingContext
-						.getShardingItem(), new SimpleDateFormat("HH:mm:ss")
-						.format(new Date()), Thread.currentThread().getId(),
-				"DATAFLOW PROCESS"));
-		for (Foo each : data) {
-			fooRepository.setCompleted(each.getId());
-		}
-	}
+    @Override
+    public List<Foo> fetchData(final ShardingContext shardingContext) {
+        logger.info(String.format(
+                "Item: %s | Time: %s | Thread: %s | %s", shardingContext
+                        .getShardingItem(), new SimpleDateFormat("HH:mm:ss")
+                        .format(new Date()), Thread.currentThread().getId(),
+                "DATAFLOW FETCH"));
+        return fooRepository.findTodoData(
+                shardingContext.getShardingParameter(), 10);
+    }
+
+    @Override
+    public void processData(final ShardingContext shardingContext,
+                            final List<Foo> data) {
+        logger.info(String.format(
+                "Item: %s | Time: %s | Thread: %s | %s", shardingContext
+                        .getShardingItem(), new SimpleDateFormat("HH:mm:ss")
+                        .format(new Date()), Thread.currentThread().getId(),
+                "DATAFLOW PROCESS"));
+        for (Foo each : data) {
+            fooRepository.setCompleted(each.getId());
+        }
+    }
 }
--- src/main/java/com/demo/job/SpringSimpleJob.java
+++ src/main/java/com/demo/job/SpringSimpleJob.java
@@ -16,21 +16,21 @@ import com.demo.process.FooRepository;
 
 public class SpringSimpleJob implements SimpleJob {
 
-	Logger logger = LogManager.getLogger(SpringSimpleJob.class);
+    Logger logger = LogManager.getLogger(SpringSimpleJob.class);
 
-	@Resource
-	private FooRepository fooRepository;
+    @Resource
+    private FooRepository fooRepository;
 
-	@Override
-	public void execute(final ShardingContext shardingContext) {
-		logger.info(String.format("Item: %s | Time: %s | Thread: %s | %s",
-				shardingContext.getShardingItem(), new SimpleDateFormat(
-						"HH:mm:ss").format(new Date()), Thread.currentThread()
-						.getId(), "SIMPLE"));
-		List<Foo> data = fooRepository.findTodoData(
-				shardingContext.getShardingParameter(), 10);
-		for (Foo each : data) {
-			fooRepository.setCompleted(each.getId());
-		}
-	}
+    @Override
+    public void execute(final ShardingContext shardingContext) {
+        logger.info(String.format("Item: %s | Time: %s | Thread: %s | %s",
+                shardingContext.getShardingItem(), new SimpleDateFormat(
+                        "HH:mm:ss").format(new Date()), Thread.currentThread()
+                        .getId(), "SIMPLE"));
+        List<Foo> data = fooRepository.findTodoData(
+                shardingContext.getShardingParameter(), 10);
+        for (Foo each : data) {
+            fooRepository.setCompleted(each.getId());
+        }
+    }
 }
